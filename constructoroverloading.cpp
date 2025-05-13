16c78f6983f23d232845686c10e27df220e98448
paw2kk <wndxakk@gmail.com>
2025-05-13 10:55:35 +0700

    menambah fungsi pemanggik

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 class mahasiswa{
-    private;
+    private:
     int nim;
     string nama;
     public:
@@ -33,5 +33,17 @@ mahasiswa::mahasiswa(int iNim, string iNama){
 
 void mahasiswa::cetak(){
     cout << endl << "Nim = "<< nim << endl;
-    cout << "Nama = " << Nama << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1(102030);             //Hanya Nim
+    mahasiswa mhs2("Andi");             //Hanya Nama
+    mahasiswa mhs3(010203, "Santi");    //Nim dan Nama
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+    
+    return 0;
 }
