160abf705ed894a19e7489b1cf23c4ddea847b90
paw2kk <wndxakk@gmail.com>
2025-05-13 10:46:42 +0700

    menyelesaikan error

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -7,7 +7,7 @@ class mahasiswa {
     mahasiswa(int nim, string nama);  //Constructir dengan parameter
 };
 //Definisi constructor dengan parameter
-mahasiswa ::mahasiswa(int nim, int nama) {
+mahasiswa::mahasiswa(int nim, string nama) {
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
     cout << "NIM    : " << endl;
     cout << "Nama   : " << endl;
