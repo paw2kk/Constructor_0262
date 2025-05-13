d6f1c1b1c10117053744d4ff14bbc111d73ac908
paw2kk <wndxakk@gmail.com>
2025-05-13 10:46:21 +0700

    mendefinisikan setiap constructor

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -13,3 +13,20 @@ class mahasiswa{
     mahasiswa(int iNim, string iNama);
     void cetak();
 };
+
+//mendefinisikan setiap constructor ngapain aja
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa::mahasiswa(int iNim){
+    nim=iNim;
+}
+
+mahasiswa::mahasiswa(string iNama){
+    nama=iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+    nim=iNim;
+    nama=iNama;
+}
