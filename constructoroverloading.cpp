ef450258e56e0449d0704f9cde90368b4a536224
paw2kk <wndxakk@gmail.com>
2025-05-13 10:51:36 +0700

    membuat program output

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -30,3 +30,8 @@ mahasiswa::mahasiswa(int iNim, string iNama){
     nim=iNim;
     nama=iNama;
 }
+
+void mahasiswa::cetak(){
+    cout << endl << "Nim = "<< nim << endl;
+    cout << "Nama = " << Nama << endl;
+}
